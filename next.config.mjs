179e2fe1bbbcc36618c5bb2e24ddd247d5a98445
mtsnrtkhr <mtsnrtkhr+github@gmail.com>
2024-08-17 12:35:45 +0900

    chore: Update next.config.mjs and deploy.yml for GitHub Pages deployment
    - Modify next.config.mjs:
      - Set assetPrefix to process.env.NEXT_PUBLIC_BASE_PATH || ''
      - Remove basePath setting
    - Update deploy.yml:
      - Set NEXT_PUBLIC_BASE_PATH environment variable in the build step to /${{ github.event.repository.name }}

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,8 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? './' : '',
+  //assetPrefix: process.env.NODE_ENV === 'production' ? './' : '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || './',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   trailingSlash: true,
 
