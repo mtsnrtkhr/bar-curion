fcfb9488cdd430d21d264ce4712cb27fb509ecde
mtsnrtkhr <mtsnrtkhr+github@gmail.com>
2024-08-17 13:48:45 +0900

    assetPrefix test

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,9 @@ const nextConfig = {
   },
   output: 'export',
   //assetPrefix: process.env.NODE_ENV === 'production' ? './' : '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || './',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  //assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || './',
+  assetPrefix: '/bar-curion',
+  //basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   trailingSlash: true,
 
 
